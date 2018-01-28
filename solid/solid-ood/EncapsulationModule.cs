c365e04553a1bf46cc989fc2c4f41f47f3ac3f3f
Kostya Vyrodov <kostya.vyrodov@gmail.com>
2018-01-28 16:46:12 +0200

    Change structure of repository

---
+++ solid/solid-ood/EncapsulationModule.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Odbc;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolidOod
+{
+    public class FileStore
+    {
+        public string WorkingDirectory { get; set; }
+
+        public void Save(int id, string message)
+        {
+            var path = GetFileName(id);
+            File.WriteAllText(path, message);
+        }
+
+        public string Read(int id)
+        {
+            // No events, no void
+            var path = GetFileName(id);
+            var msg = File.ReadAllText(path);
+            return msg;
+        }
+
+        public string GetFileName(int id)
+        {
+            return Path.Combine(this.WorkingDirectory, id + ".txt");
+        }
+    }
+}
