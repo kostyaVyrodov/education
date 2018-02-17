9d9446ce4489cd3691fcb126baa2382891840257
Kostya Vyrodov <kostya.vyrodov@gmail.com>
2018-02-17 13:59:28 +0200

    Implement part of chapter about proof of work

---
+++ algorithms/block-chain/building-blockchain/src/proof-of-work.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"fmt"
+	"math"
+	"math/big"
+	"strconv"
+)
+
+var (
+	maxNonce = math.MaxInt64
+)
+
+// It's the block header storing the difficulty at which the block was mined.
+const targetBits = 24
+
+type ProofOfWork struct {
+	block  *Block
+	target *big.Int
+}
+
+func NewProofOfWork(b *Block) *ProofOfWork {
+	target := big.NewInt(1)
+	target.Lsh(target, uint(256-targetBits))
+
+	pow := &ProofOfWork{b, target}
+	return pow
+}
+
+// nonce is counter
+func (pow *ProofOfWork) prepareData(nonce int) []byte {
+	data := bytes.Join(
+		[][]byte{
+			pow.block.PrevBlockHash,
+			pow.block.Data,
+			[]byte(strconv.FormatInt(pow.block.Timestamp, 10)),
+			[]byte(strconv.FormatInt(targetBits, 10)),
+			[]byte(strconv.FormatInt(int64(nonce), 10)),
+		},
+		[]byte{},
+	)
+
+	return data
+}
+
+func (pow *ProofOfWork) Run() (int, []byte) {
+	var hashInt big.Int
+	var hash [32]byte
+	nonce := 0
+
+	fmt.Printf("Mining the block containing \"s\"\n", pow.block.Data)
+
+	for nonce < maxNonce {
+		data := pow.prepareData(nonce)
+		hash = sha256.Sum256(data)
+		fmt.Printf("\r%x", hash)
+		hashInt.SetBytes(hash[:])
+		if hashInt.Cmp(pow.target) == -1 {
+			break
+		} else {
+			nonce++
+		}
+		fmt.Print("\n\n")
+
+	}
+
+	return nonce, hash[:]
+}
