73cccd731563fa0486f2e03fc0657efe8a5355bf
Kostya Vyrodov <kostya.vyrodov@gmail.com>
2018-02-18 09:45:10 +0200

    Remove unused files

--- algorithms/block-chain/building-blockchain/src/module3/blockchain.go
+++ algorithms/block-chain/building-blockchain/src/module3/blockchain.go
@@ -1,6 +1,10 @@
 package main
 
+import "github.com/boltdb/bolt"
+
 // In its essence blockchain is just a database with certain structure: it’s an ordered, back-linked list.
+const dbFile = "blockchain.db"
+const blocksBucket = "blocks"
 
 type Blockchain struct {
 	blocks []*Block
@@ -13,5 +17,25 @@ func (bc *Blockchain) AddBlock(data string) {
 }
 
 func NewBlockchain() *Blockchain {
-	return &Blockchain{[]*Block{NewGenesisBlock()}}
+	var tip []byte
+	db, err := bolt.Open(dbFile, 0600, nil)
+
+	err = db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(blocksBucket))
+
+		if b == nil {
+			genesis := NewGenesisBlock()
+			b, err := tx.CreateBucket([]byte(blocksBucket))
+			err = b.Put(genesis.Hash, genesis.Serialize())
+			err = b.Put([]byte("l"), genesis.Hash)
+			tip = genesis.Hash
+		} else {
+			tip = b.Get([]byte("l"))
+		}
+
+		return nil
+	})
+
+	bc := Blockchain{tip, db}
+	return &bc
 }
