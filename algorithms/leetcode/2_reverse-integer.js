8f0eee804acc4287ec24c44f303832cc3032d2c8
Kostya Vyrodov <kostya.vyrodov@gmail.com>
2021-03-28 17:44:32 +0300

    Adjusted reverse

--- algorithms/leetcode/2_reverse-integer.js
+++ algorithms/leetcode/2_reverse-integer.js
@@ -1,24 +1,15 @@
 var reverse = function (x) {
-    let isNegative = false;
-    let maxInt = Math.pow(2, 31) - 1 
-    if (x < 0) {
-        isNegative = true
-        x = x * -1
-    }
+    const maxInt = Math.pow(2, 31) - 1
+    const minInt = Math.pow(-2, 31)
 
-    let res = 0;
-    while (x > 0) {
-        const tail = x % 10
+    let res = 0
+
+    while (x != 0) {
+        const pop = x % 10
         res *= 10
-        res += tail
-        x = Math.floor(x / 10)
-    }
-    debugger;
-    if (isNegative) {
-        return res > maxInt ? 0 : (res * -1);
+        res += pop
+        x = Math.trunc(x / 10)
     }
 
-    return res > maxInt ? 0 : res;
+    return (minInt < res && res < maxInt) ? res : 0;
 };
-
-reverse(123)
