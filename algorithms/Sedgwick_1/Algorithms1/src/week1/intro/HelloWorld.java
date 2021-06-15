855c52e1437bb07d6d076826cab014d3d0a3c64e
Kostya Vyrodov <kostya.vyrodov@gmail.com>
2021-06-15 10:18:58 +0300

    Delete all files from notes

--- algorithms/Sedgwick_1/Algorithms1/src/week1/intro/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package week1.intro;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World");
-    }
-}
--- algorithms/js/greedy-coin-change.js
+++
@@ -1,5 +0,0 @@
-// Problem Statement: Given a value V, we want to make changes for V Rupees.
-// We have infinite supply of each of the denominations in Indian currency, i.e.
-// we have infinite supply of {1, 2, 5, 10, 20, 50, 100, 500, 1000} valued coins/notes
-// which is the minimum number of coins and/or notes needed to make the changes 
-// for V Rs
